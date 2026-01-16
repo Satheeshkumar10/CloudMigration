9a0f0b57c48d169eaa176483f9c288911bdec245
Satheeshkumar10 <92844306+Satheeshkumar10@users.noreply.github.com>
2026-01-16 11:27:08 +0530

    removed unwanted package

--- UITEST/Appium/SfButtonSample/SfButtonSample/Platforms/iOS/AppDelegate.cs
+++ UITEST/Appium/SfButtonSample/SfButtonSample/Platforms/iOS/AppDelegate.cs
@@ -5,21 +5,6 @@ namespace SfButtonSample;
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-    //protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-    protected override MauiApp CreateMauiApp()
-    {
-        //throw new NotImplementedException();
-
-
-
-
-
-#if ENABLE_TEST_CLOUD
-                Xamarin.Calabash.Start();
-#endif
-        return MauiProgram.CreateMauiApp();
-
-
-
-    }
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
+
